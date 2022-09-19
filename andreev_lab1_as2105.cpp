dec5ce1f6742543e1bb6873f1f80dac06cd26a21
kinokiken <figoroki@mail.ru>
2022-09-19 23:37:48 +0300

    add pipe
    немного упростил код и добавил возможность добавлять характеристики трубы с условиями ввода

--- andreev_lab1_as2105.cpp
+++ andreev_lab1_as2105.cpp
@@ -1,15 +1,74 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-struct pipe
+struct tubes
 {
-    float length = 0;
-    float diameter = 0;
-    bool repair = false;
+    float length;
+    float diameter;
+    bool repair;
 };
 
-main()
+struct CStation 
 {
-    
+    string name;
+    int workshops;
+    int WorkingNum;
+    float efficiency;
+};
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    tubes pipe =  {};
+    CStation cs = {};
+    int MenuChoice = -1;
+
+    while (MenuChoice)
+    {
+    cout << " 1. Добавить трубу \n 2. Добавить КС \n 3. Просмотр всех объектов \n 4. Редактировать трубу \n 5. Редактировать КС \n 6. Сохранить \n 7. Загрузить \n 0. Выход"<<endl;
+    cin >> MenuChoice;
+
+    if (MenuChoice == 1)
+    {
+        cout << "Задайте длину трубы:" << endl;
+        cin >> pipe.length;
+        while (!pipe.length)
+        {
+            cout << "Задайте корректную длину трубы:" << endl;
+            cin.clear();
+            cin.ignore(INT_MAX,'\n');
+            cin >> pipe.length;
+        }
+
+        cout << "Задайте диаметр трубы:" << endl;
+        cin >> pipe.diameter;
+        while (!pipe.diameter)
+        {
+            cout << "Задайте корректный диаметр трубы:" << endl;
+            cin.clear();
+            cin.ignore(INT_MAX,'\n');
+            cin >> pipe.diameter;
+        }
+
+        cout << "Находится ли эта труба в ремонте? 0-да, 1-нет" << endl;
+        while (!(cin>>pipe.repair))
+        {
+            cout << "Введите 0, если труба находится в ремонте; 1, если труба исправна" << endl;
+            cin.clear();
+            cin.ignore(INT_MAX,'\n');
+        }
+
+    cout << "\n Вы успешно задали параметры трубы, возвращаем Вас в меню" << endl << endl;
+
+    }
+    else
+    {
+        break;
+    }
+    }
+
+    return 0;
 }
